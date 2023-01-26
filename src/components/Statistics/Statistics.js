edcc48147370e0c921e8e4353729911602a1f498
aslantasezgin <aslantasezginn@gmail.com>
2023-01-26 21:17:37 +0300

    ui finished

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,45 @@
+
+
+import './Statistics.css'
+
+
+const StatisticsJSON = [
+    {
+       price:"321.4",
+       icon:"fa-solid fa-basket-shopping",
+       title:"Sales",
+       backgroundColor:"#6571FF"
+    },
+    {
+        price:"137896.6",
+        icon:"fa-solid fa-hand-holding-dollar",
+        title:"Purchases",
+        backgroundColor:"#0AC074"
+     },
+
+     {
+        price:"12.3",
+        icon:"fa-solid fa-arrow-right",
+        title:"",
+        backgroundColor:""
+     },
+
+     {
+        price:"",
+        icon:"",
+        title:"",
+        backgroundColor:""
+     },
+]
+
+
+const Statistics = () => {
+return(
+    <div className="statistics-wrapper">
+
+    </div>
+)
+
+}
+
+export default Statistics
---
+++ src/components/StatisticsBox/StatisticsBox.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+const StatisticsBox = () => {
+return(
+    
+<div className="statistics-box">
+    <div className="icon-box"></div>
+    <div className="statistics-text">
+       <h2>$ 321.4B</h2> 
+       <p>Sales</p>
+    </div>
+</div>
+)
+}
+
+
+export default StatisticsBox
